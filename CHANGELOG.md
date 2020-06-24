9cc179e0e56bafb03ebd72bcf6794646225d50b0
Stjepan Glavina <stjepang@gmail.com>
2020-06-24 02:24:48 +0200

    Bump to v1.1.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# Version 1.1.4
+
+- Replace `usize::MAX` with `std::usize::MAX`.
+
 # Version 1.1.3
 
 - Update dependencies.
--- src/lib.rs
+++ src/lib.rs
@@ -44,6 +44,7 @@ use std::process;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
 use std::time::{Duration, Instant};
+use std::usize;
 
 use event_listener::Event;
 
