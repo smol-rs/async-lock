f9b304d3db8e2039f69b1198e9323d6ea8340994
Stjepan Glavina <stjepang@gmail.com>
2020-08-29 15:14:57 +0200

    Add semaphore and barrier

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
 //! Async locking primitives.
 //!
-//! This crate provides two primitives:
+//! This crate provides the following primitives:
 //!
+//! * [`Barrier`] - enables tasks to synchronize all together at the same time.
 //! * [`Mutex`] - a mutual exclusion lock.
 //! * [`RwLock`] - a reader-writer lock, allowing any number of readers or a single writer.
+//! * [`Semaphore`] - limits the number of concurrent operations.
 
 #![warn(missing_docs, missing_debug_implementations, rust_2018_idioms)]
 
 #[doc(inline)]
-pub use {
-    async_mutex::{Mutex, MutexGuard},
-    async_rwlock::{RwLock, RwLockReadGuard, RwLockWriteGuard},
-};
+pub use {async_barrier::*, async_mutex::*, async_rwlock::*, async_semaphore::*};
