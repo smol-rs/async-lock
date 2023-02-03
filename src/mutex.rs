39e0b6dc75bf577286be2d4848995cf4c18c8139
Caio <c410.f3r@gmail.com>
2023-02-03 20:04:57 -0300

    Mark `MutexGuard` with `#[clippy::has_significant_drop]` (#37)
    * Mark `MutexGuard` with `#[clippy::has_significant_drop]`
    
    `#[clippy::has_significant_drop]` tells that a structure should be considered when evaluating some lints. Examples of such behavior are the existent `clippy::significant_drop_in_scrutinee` and in the soon-to-be-finished https://github.com/rust-lang/rust-clippy/issues/9399.
    
    * Include more structures

--- src/mutex.rs
+++ src/mutex.rs
@@ -536,6 +536,7 @@ impl<T: ?Sized, B: Borrow<Mutex<T>>> Drop for AcquireSlow<B, T> {
 }
 
 /// A guard that releases the mutex when dropped.
+#[clippy::has_significant_drop]
 pub struct MutexGuard<'a, T: ?Sized>(&'a Mutex<T>);
 
 unsafe impl<T: Send + ?Sized> Send for MutexGuard<'_, T> {}
@@ -595,6 +596,7 @@ impl<T: ?Sized> DerefMut for MutexGuard<'_, T> {
 }
 
 /// An owned guard that releases the mutex when dropped.
+#[clippy::has_significant_drop]
 pub struct MutexGuardArc<T: ?Sized>(Arc<Mutex<T>>);
 
 unsafe impl<T: Send + ?Sized> Send for MutexGuardArc<T> {}
